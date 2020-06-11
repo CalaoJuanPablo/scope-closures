02f1a92097fcfec270807a9ee59b9d8b6fba1d9c
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-06-11 09:43:04 -0500

    global scope

---
+++ src/scope/global.js
@@ -0,0 +1,11 @@
+var hello = 'Hello'
+let world = 'Hello world'
+const helloWorld = 'Hello World CONST'
+
+function anotherFunction() {
+	console.log(hello)
+	console.log(world)
+	console.log(helloWorld)
+}
+
+anotherFunction()
